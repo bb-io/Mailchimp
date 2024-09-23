e543d6b755c71b09825b55d3d66e4c873373b3a8
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-09-23 14:53:36 +0300

    Fixed template data source

--- Apps.Mailchimp/Models/Responses/Templates/TemplatesResponse.cs
+++ Apps.Mailchimp/Models/Responses/Templates/TemplatesResponse.cs
@@ -1,9 +1,10 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Newtonsoft.Json;
 
 namespace Apps.Mailchimp.Models.Responses.Templates;
 
 public class TemplatesResponse : BaseSearchResponse<TemplateResponse>
 {
-    [Display("Templates")]
+    [Display("Templates"), JsonProperty("templates")]
     public override List<TemplateResponse> Items { get; set; } = new();
 }
